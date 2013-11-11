968c79aeb6303f7daf9dc529a61520dbb4aa4de9
Tomohiro Suwa（tsuwatch） <neoen.gsn@gmail.com>
2013-11-11 18:33:22 +0900

    add background.js

---
+++ background.js
@@ -0,0 +1,21 @@
+var parentId = chrome.contextMenus.create({
+  "title": "選択した動画を作成するマイリストに追加する",
+  "type": "normal",
+  "contexts": ["link"],
+  "onclick": addNicovideo()
+});
+
+function addNicovideo() {
+  return function(info, tab) {
+    if (info.linkUrl.match(new RegExp("https?:\/\/.*(?:nicovideo\.jp|nico\.ms).*\/sm([0-9]+)"))) {
+      videoId = RegExp.$1;
+      var key = localStorage.length;
+      localStorage.setItem(key, videoId);
+      for (var i=0; i<localStorage.length; i++) {
+        alert(localStorage[i]);
+      }
+    } else {
+      alert('ニコニコ動画の動画ではないか，未対応のURLです');
+    }
+  }
+}
